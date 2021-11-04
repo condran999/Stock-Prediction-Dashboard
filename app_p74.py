522cf1e5297ccfb5c8858d13ae7c8ee0d07e432e
Condran Fernandes <81804267+condran999@users.noreply.github.com>
2021-11-04 17:01:57 +0530

    Update app_p74.py

--- app_p74.py
+++ app_p74.py
@@ -420,5 +420,10 @@ def main():
     
     
 if __name__ == '__main__':
-    main()
+       import warnings
+    warnings.warn("use 'python -m nltk', not 'python -m nltk.downloader'",         DeprecationWarning)
+    app.run_server(debug=True)
+#     main()
+
+    
 
